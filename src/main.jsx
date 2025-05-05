b19968ff0c8d24630c1aa86ec80611cb107c17b5
Bojan Lukic <bojan.ognjen@gmail.com>
2025-05-05 15:11:50 +0200

    First React component implemented and first variable used in it

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const title = 'React.js';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App title = {title}/>
   </StrictMode>,
 )
