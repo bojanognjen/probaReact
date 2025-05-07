388dd26e90717d4be67b1b9548729a8deaed0412
Bojan Lukic <bojan.ognjen@gmail.com>
2025-05-07 17:40:36 +0200

    Rendering elements of list using .map() function in JSX!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,49 @@
-function App(props) {
+const list = [
+  {
+    title: "React",
+    url: "https://react.dev/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: "Instagram",
+    url: "www.instagram.com",
+    author: "Bojan Lukic",
+    num_comments: 5,
+    points: 2,
+    objectID: 2,
+  }
+];
+
+function App() {
   return (
     <div>
-      <h1>Hello {props.title}</h1>
+      <h1>My hacker stories</h1>
+
+      <hr />
+
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
