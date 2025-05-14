64f5041d17291df23f221217b829782f8ad2f6bd
Bojan Lukic <bojan.ognjen@gmail.com>
2025-05-14 20:52:41 +0200

    Adding props to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,34 @@
-import { createRoot } from 'react-dom/client'
+import { useState } from 'react';
 
-const firstList = [
-  {
-    title: "React",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Instagram",
-    url: "www.instagram.com",
-    author: "Bojan Lukic",
-    num_comments: 5,
-    points: 2,
-    objectID: 2,
-  }
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://react.dev/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "Instagram",
+      url: "www.instagram.com",
+      author: "Bojan Lukic",
+      num_comments: 5,
+      points: 2,
+      objectID: 2,
+    }
+  ];
 
-const secondList = [
-  {
-    title: "Zorana",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Danica",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Ljilja",
-    url: "www.instagram.com",
-    author: "Bojan Lukic",
-    num_comments: 5,
-    points: 2,
-    objectID: 2,
-  }
-];
-
-const thirdList = [
-  {
-    title: "Sasa",
-    url: "https://react.dev/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Stojan",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Dragana",
-    url: "www.instagram.com",
-    author: "Bojan Lukic",
-    num_comments: 5,
-    points: 2,
-    objectID: 2,
-  }
-];
-
-const App = () => (
+  return (
     <div>
       <h1>My hacker stories</h1>
 
@@ -89,38 +36,141 @@ const App = () => (
 
       <hr />
 
-      <List list={firstList}/>
-      <List list={secondList}/>
-      <List list={thirdList}/>
+      <List list={stories}/>
+      <hr />
+
+      <Counter />
+
+      <hr />
+
+      <TextHandler />
     </div>
   );
+}
 
-const List = ({list}) => (
+const List = (props) => (
     <ul>
-        {list.map((item) => (
-          <Item item={item}/>
+        {props.list.map((item) => (
+          <Item key={item.objectID} item={item}/>
         ))}
     </ul>
   );
 
 
-const Item = ({ item }) => (
+const Item = (props) => (
     <li>
       <span>
-        <a href={item.url}>{item.title}</a>
+        <a href={props.item.url}>{props.item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
+      <span>{props.item.author}</span>
+      <span>{props.item.num_comments}</span>
+      <span>{props.item.points}</span>
     </li>
   );
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event)
+
+    console.log(event.target.value)
+  }
+  
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
     </div>
   );
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    setCount(count + 1)
+  }
+
+  return (
+    <div>
+      <span>Count: {count}</span>
+      <button onClick={handleClick}>
+        Increase count
+      </button>
+    </div>
+  )
+}
+
+const other = () => console.log("Some other activity happened.")
+
+  const TextHandler = () => {
+    const [text, setText] = useState('')
+
+    const handleChange = (event) => {
+      setText(event.target.value)
+    }
+
+    return(
+      <div>
+        <input type="text" value={text} onChange={handleChange} onBlur={other}/>
+
+        {text}
+      </div>
+    )
+  }
+
 
 
 export default App;
+
+// const secondList = [
+//   {
+//     title: "Zorana",
+//     url: "https://react.dev/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Danica",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+//   {
+//     title: "Ljilja",
+//     url: "www.instagram.com",
+//     author: "Bojan Lukic",
+//     num_comments: 5,
+//     points: 2,
+//     objectID: 2,
+//   }
+// ];
+
+// const thirdList = [
+//   {
+//     title: "Sasa",
+//     url: "https://react.dev/",
+//     author: "Jordan Walke",
+//     num_comments: 3,
+//     points: 4,
+//     objectID: 0,
+//   },
+//   {
+//     title: "Stojan",
+//     url: "https://redux.js.org/",
+//     author: "Dan Abramov, Andrew Clark",
+//     num_comments: 2,
+//     points: 5,
+//     objectID: 1,
+//   },
+//   {
+//     title: "Dragana",
+//     url: "www.instagram.com",
+//     author: "Bojan Lukic",
+//     num_comments: 5,
+//     points: 2,
+//     objectID: 2,
+//   }
+// ];
